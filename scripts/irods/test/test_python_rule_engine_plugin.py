1fa183eab7d1fc9e389f89abe6a6d5fd2c5b0091
Martin Jaime Flores Jr <martinflores@renci.org>
2024-06-14 16:10:14 -0500

    [irods/irods_rule_engine_plugin_python_204] Test variable handling
    This commit implements a test to show that we properly 'blank' a
    misbehaving variable.

--- scripts/irods/test/test_python_rule_engine_plugin.py
+++ scripts/irods/test/test_python_rule_engine_plugin.py
@@ -108,3 +108,29 @@ class Test_Python_Rule_Engine_Plugin(resource_suite.ResourceBase, unittest.TestC
                       '''writeLine("stdout", error("SYS_INVALID_FILE_PATH") == errorcode(myrule()))''',
                       'null', 'ruleExecOut' ],
                     'STDOUT_SINGLELINE',['true'] )
+
+    @unittest.skipUnless(plugin_name == 'irods_rule_engine_plugin-python', 'only applicable for python REP')
+    def test_prep_blanks_out_variable__issue_204(self):
+        with append_native_re_to_server_config():
+            with temporary_core_file(plugin_name=IRODS_RULE_LANGUAGE_RULE_ENGINE_PLUGIN_NAME) as core_re,\
+                 temporary_core_file(plugin_name=PYTHON_RULE_ENGINE_PLUGIN_NAME) as core_py:
+                core_py.add_rule(dedent('''
+                def pep_api_data_obj_put_pre(rule_args,callback,rei):
+                    import irods_errors
+                    callback.writeLine("serverLog", ":python PEP:")
+                    return irods_errors.RULE_ENGINE_CONTINUE
+                '''))
+                core_re.add_rule(dedent('''
+                pep_api_data_obj_put_pre(*A,*B,*C,*D,*E) {
+                    writeLine("serverLog", ":native PEP:")
+                }
+                '''))
+                time.sleep(5);
+                initial_size_of_server_log = lib.get_file_size_by_path(paths.server_log_path())
+                tmpf = tempfile.NamedTemporaryFile()
+                self.admin.assert_icommand( ['iput',tmpf.name] )
+                lib.delayAssert(
+                    lambda: lib.log_message_occurrences_equals_count(
+                        msg=':native PEP:',
+                        count=1,
+                        start_index=initial_size_of_server_log),maxrep=5)
