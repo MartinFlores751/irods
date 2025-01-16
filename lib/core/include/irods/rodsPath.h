29e144936aac81de54ba495b456aec3655af3a8a
Martin Jaime Flores Jr. <martin.flores98@tutanota.com>
2025-01-16 01:18:44 -0600

    [???] Add helper to clear `rodsPathInp_t`

--- lib/core/include/irods/rodsPath.h
+++ lib/core/include/irods/rodsPath.h
@@ -69,6 +69,7 @@ int
 getFileType( rodsPath_t *rodsPath );
 void
 clearRodsPath( rodsPath_t *rodsPath );
+auto clearRodsPathInp( rodsPathInp_t* path ) -> void;
 
 // Returns a new path with the following special characters escaped:
 //   - '\f'
