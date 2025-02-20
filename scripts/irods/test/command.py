703887a694cb27531b9207a8e6dd3df51b2f1595
Martin Jaime Flores Jr. <martinflores@renci.org>
2025-02-20 22:08:10 +0000

    Add missing import

--- scripts/irods/test/command.py
+++ scripts/irods/test/command.py
@@ -98,6 +98,7 @@ def _assert_helper(command_arg, check_type='EMPTY', expected_results='', use_reg
         if desired_rc != rc:
             print('RETURN CODE CHECK FAILED')
             result = False
+    import signal
     if rc == -signal.SIGSEGV or rc < 0:
         print('!!!THERE WAS A SEGFAULT!!!')
         print('return code: [{0}]'.format(rc))
